aa0b970630e9cccabdf74ffc3e7f566f00664052
Folareen <sakawahab03@gmail.com>
2022-03-25 02:06:55 +0100

    trying out some js syntaxes

--- home.js
+++ home.js
@@ -2,25 +2,23 @@ const SIGN_IN = document.querySelector('.overlay-container .sign-in');
 const SIGN_UP = document.querySelector('.overlay-container .sign-up');
 const overlayContainer = document.querySelector('.overlay-container');
 const formContainer = document.querySelector('.form-container');
-SIGN_IN.addEventListener('click', function (){
-    overlayContainer.style.bottom = "0";
-    formContainer.style.top = "-75%";
-    overlayContainer.style.zIndex = "1";
-    formContainer.style.zIndex = "2";
-});
+
+if (window.getComputedStyle(document.querySelector('.container')).width == "300px"){
+    SIGN_IN.addEventListener('click', function (){
+        overlayContainer.style.bottom = "0";
+        formContainer.style.top = "-75%";
+        overlayContainer.style.zIndex = "1";
+        formContainer.style.zIndex = "2";
+    });
+    SIGN_UP.addEventListener('click', function (){
+        overlayContainer.style.bottom = "-25%";
+        formContainer.style.top = "0";
+        overlayContainer.style.zIndex = "2";
+        formContainer.style.zIndex = "1";
+    });
+}
+
 // window.addEventListener('DOMContentLoaded', function(){
 //     formContainer.style.left = "-50%";
 //     overlayContainer.style.right = "-50%"
 // });
-SIGN_UP.addEventListener('click', function (){
-    overlayContainer.style.bottom = "-25%";
-    formContainer.style.top = "0";
-    overlayContainer.style.zIndex = "2";
-    formContainer.style.zIndex = "1";
-});
-
-// if (document.querySelector('.container').style.width == 500){
-//     alert("yayyy")
-// }
-
-// console.log(document.querySelector('.container').style.width)
