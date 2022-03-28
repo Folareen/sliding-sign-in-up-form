7bd85f968b8194b786224ab799b38efafe2ad63d
Folareen <sakawahab03@gmail.com>
2022-03-28 07:12:52 +0100

    responsiveness bugs fixed

--- home.js
+++ home.js
@@ -3,22 +3,36 @@ const SIGN_UP = document.querySelector('.overlay-container .sign-up');
 const overlayContainer = document.querySelector('.overlay-container');
 const formContainer = document.querySelector('.form-container');
 
-if (window.getComputedStyle(document.querySelector('.container')).width == "300px"){
-    SIGN_IN.addEventListener('click', function (){
+SIGN_IN.addEventListener('click', signIn);
+SIGN_UP.addEventListener('click', signUp);
+
+function signIn(){
+    if (window.getComputedStyle(document.querySelector('.container')).width == "300px"){
         overlayContainer.style.bottom = "0";
         formContainer.style.top = "-75%";
         overlayContainer.style.zIndex = "1";
         formContainer.style.zIndex = "2";
-    });
-    SIGN_UP.addEventListener('click', function (){
+    }
+    else{
+        overlayContainer.style.left = "50%";
+        formContainer.style.left = "-50%";
+        overlayContainer.style.zIndex = "1";
+        formContainer.style.zIndex = "2";
+    }
+};
+
+function signUp(){
+    if (window.getComputedStyle(document.querySelector('.container')).width == "300px"){
         overlayContainer.style.bottom = "-25%";
         formContainer.style.top = "0";
         overlayContainer.style.zIndex = "2";
         formContainer.style.zIndex = "1";
-    });
-}
+    }
+    else{
+        overlayContainer.style.left = "-50%";
+        formContainer.style.left = "50%";
+        overlayContainer.style.zIndex = "2";
+        formContainer.style.zIndex = "1";
+    }
 
-// window.addEventListener('DOMContentLoaded', function(){
-//     formContainer.style.left = "-50%";
-//     overlayContainer.style.right = "-50%"
-// });
+}
